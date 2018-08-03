797d1b7a7fd5aae80c660bbcf910f590c32ebbb7
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 06:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function happyHolidayTo (holiday, name) {
+function holidayCountdown (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
